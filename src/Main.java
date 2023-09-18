29a777a2b5e30dab3031fc9563e44aecf7a2c01d
Андрей <yarm.99@yandex.ru>
2023-09-18 16:31:27 +0300

    Add Snake and Fox

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,4 @@
-import Insides.Animal.Animal;
-import Insides.Animal.Herbivoress.Rabbit;
-import Insides.Animal.Predators.Snake;
-import Insides.Animal.Predators.Wolf;
-import Insides.Plant.Plant;
 import Source.Island;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Random;
-import java.util.concurrent.ThreadLocalRandom;
-
-import static Source.Island.islandModel;
 
 public class Main {
     public static void main(String[] args) {
